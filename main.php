2bfd58d86bcd98f4101b58a002c75e37e31a697c
Malinamaste <morey.melanie@gmail.com>
2018-12-17 16:53:55 +0100

    integration

---
+++ main.php
@@ -0,0 +1,23 @@
+<section id="presentation">
+	<article class="container">
+		<h1 class="ml16">Made with love</h1>
+	</article>
+</section>
+
+<div class="container">
+
+	<section id="best-products">
+		<h2>Our best sellers</h2>
+		<article>
+			<img src="images/fond smoothie.png" alt="feuillage">
+			<img src="images/pinapo.jpg" alt="Pinapo smoothie">
+		</article>
+		<article>
+			<img src="images/fond smoothie.png" alt="feuillage">
+		</article>
+		<article>
+			<img src="images/fond smoothie.png" alt="feuillage">
+		</article>
+	</section>
+
+</div>
