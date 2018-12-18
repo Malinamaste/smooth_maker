f3b6480fbba70a1bccb753fcc97f6070affe0037
Malinamaste <morey.melanie@gmail.com>
2018-12-18 09:52:43 +0100

    modif inte

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<!-- Flat Icon -->
 		<link rel="icon" type="image/png" href="images/fruit-juice.png" />
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<!-- Font Awesome -->
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 		<!-- Google Fonts -->
@@ -75,6 +77,9 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
 	<!-- Text Animation -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+	<!-- Bootstrap -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	<!-- JS Perso -->
 	<script type="text/javascript" src="js/main.js"></script>
 	</body>
--- main.php
+++ main.php
@@ -1,23 +1,14 @@
-<section id="presentation">
-	<article class="container">
-		<h1 class="ml16">Made with love</h1>
+<section id="main-background">
+	<article>
+		<!-- slider -->
+	</article>
+	<article>
+		<!-- best-sellers -->
+	</article>
+	<article>
+		<!-- recettes -->
+	</article>
+	<article>
+		<!-- nous trouver -->
 	</article>
 </section>
-
-<div class="container">
-
-	<section id="best-products">
-		<h2>Our best sellers</h2>
-		<article>
-			<img src="images/fond smoothie.png" alt="feuillage">
-			<img src="images/pinapo.jpg" alt="Pinapo smoothie">
-		</article>
-		<article>
-			<img src="images/fond smoothie.png" alt="feuillage">
-		</article>
-		<article>
-			<img src="images/fond smoothie.png" alt="feuillage">
-		</article>
-	</section>
-
-</div>
