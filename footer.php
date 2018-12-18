d65f2c98709ffb30c6feb057f88bf0b9b4040032
MarieLaure0610 <m.laure.faraut@gmail.com>
2018-12-18 11:33:33 +0100

    modif index

---
+++ footer.php
@@ -0,0 +1,40 @@
+<!-- FOOTER -->
+<footer>
+			<div id="footer" class="container">
+			<a href="index.php"><img src="images/logo.png" alt="Logo de Wai Smoothies"><br/>Jus de fruit 100% BIO</a>
+				<section id="map">
+					<h5>Où nous trouver</h5>
+					<!-- MAP -->
+				</section>
+				<section id="contact">
+					<h5>Contactez-nous</h5>
+					<ul>
+						<li>Wai Smoothies</li>
+						<li>34 rue des fruits</li>
+						<li>69002 Lyon</li>
+						<li>Telephone: +33 4 25 45 89 47</li>
+						<li>E-mail: <a href="#">info@wai-smoothies.com</a></li>
+					</ul>
+				</section>
+				<section id="newsletter">
+					<h5>Abonnez-vous à notre newsletter</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					<form>
+						<label></label>
+						<input type="submit" name="subscribe" value="S'INSCRIRE">
+						<button href="#"></button>
+					</form>
+				</section>
+			</div>
+		</footer>
+	<!-- Jquery -->
+	<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+	<!-- Text Animation -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+	<!-- Bootstrap -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<!-- JS Perso -->
+	<script type="text/javascript" src="js/main.js"></script>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>Wai Smoothies</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<!-- Flat Icon -->
+		<link rel="icon" type="image/png" href="images/fruit-juice.png" />
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		<!-- Reset -->
+		<link rel="stylesheet" type="text/css" href="css/reset.css" media="all" />
+		<!-- CSS Perso -->
+		<link rel="stylesheet" type="text/css" href="css/base.css" media="screen" />
+		<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+	</head>
+	<body>
+		<!-- HEADER -->
+		<header id="header">
+			<a href="index.php"><img src="images/logo.png" alt="Logo de Wai Smoothies"><br/>Jus de fruit 100% BIO</a>
+			<nav id="social">
+				<ul>
+					<li>
+						<a href="#"><i class="fab fa-facebook-f"></i></a>
+					</li>
+					<li>
+						<a href="#"><i class="fab fa-pinterest-p"></i></a>
+					</li>
+					<li>
+						<a href="#"><i class="fab fa-instagram"></i></a>
+					</li>
+				</ul>
+			</nav>
+			<nav id="main-nav">
+				<ul>
+					<li>
+						<a href="#">Notre concept</a>
+					</li>
+					<li>
+						<a href="#">Nos recettes</a>
+					</li>
+					<li>
+						<a href="#">Contact</a>
+					</li>
+				</ul>
+			</nav>
+
+		</header>
