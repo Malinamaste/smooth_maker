f79ef5ce3d08a58435c07760bbf00b2a782d953b
MarieLaure0610 <m.laure.faraut@gmail.com>
2018-12-19 11:54:54 +0100

    test

--- header.php
+++ header.php
@@ -4,13 +4,13 @@ include_once "php/classes/user.class.php";
 
 $userSession = new User();  
 
-if($userSession->isAuthenticated() == false){
-	header("Location: connexion.php");
-	exit();
-}
-else {
-	$user = $userSession->getFullName();
-}
+// if($userSession->isAuthenticated() == false){
+// 	header("Location: connexion.php");
+// 	exit();
+// }
+// else {
+// 	$user = $userSession->getFullName();
+// }
 
 ?>
 
