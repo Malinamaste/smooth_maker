9255267812d6bbf4c411d3bc1da964954ed64311
MarieLaure0610 <m.laure.faraut@gmail.com>
2018-12-19 16:50:37 +0100

    concept

--- concept.php
+++ concept.php
@@ -6,33 +6,42 @@
 
 <section id="main-background">
  <section class="about-us">
+     <section>
       <h1>Notre histoire ...</h1>
             <span>Tout est parti de notre voyage dans les tropiques où la magie est apparue :-D</span>
-            <img src="img concept/hawaii-maui-mountain-999089.jpg" alt="paysage hawaii" id="hawaii">
+        <div id="hawaii">
+            <img src="img concept/hawaii-maui-mountain-999089.jpg" alt="paysage hawaii">
+        </div>
         <div id="flex">
-            <img src="img concept/beach-celebrate-cheers-1058433.jpg" alt="smoothie rouge" id="smoothie">
-            <p>Ut tempor nisl in enim faucibus, ut venenatis purus volutpat. Vivamus vel posuere diam. In nunc ex, tempor viverra tortor a, ullamcorper elementum ligula. Donec nec turpis ullamcorper, accumsan leo at, placerat diam. Vivamus faucibus pretium pellentesque. Donec semper ut arcu vitae laoreet. Proin vitae volutpat risus. Pellentesque maximus bibendum nibh at fermentum.</p>
+            <div><img src="img concept/beach-celebrate-cheers-1058433.jpg" alt="smoothie rouge"></div>
+            <p>Ut tempor nisl in enim faucibus, ut venenatis purus volutpat. Vivamus vel posuere diam. In nunc ex, tempor viverra tortor a, ullamcorper elementum ligula. Donec nec turpis ullamcorper, accumsan leo at, placerat diam. Vivamus faucibus pretium pellentesque.</p>
         </div>
     </section>
-    <section id="concept" class="about-us">
+    <section id="concept">
        <h1>Notre concept ...</h1>
+       <div id="article">
         <article>
             <h2>Du BIO à gogo !</h2>
               <p>Ut tempor nisl in enim faucibus, ut venenatis purus volutpat. Vivamus vel posuere diam. In nunc ex, tempor viverra tortor a, ullamcorper elementum ligula. Donec nec turpis ullamcorper, accumsan leo at, placerat diam. Vivamus faucibus pretium pellentesque. Donec semper ut arcu vitae laoreet. Proin vitae volutpat risus.</p>
         </article>
         <article>
-              <img src="img concept/cleanise-decoration-flora-1030909.jpg" alt="smoothie fraise" id="smoothie2">
-        </article>
-        <article id="flex-basis">
             <h2>Des recettes ancestrales ...</h2>
               <p> Pellentesque maximus bibendum nibh at fermentum. Donec dignissim nulla id sem elementum lobortis. Suspendisse auctor tortor in tempor efficitur. Donec lacus diam, tristique at erat vitae, convallis rutrum ligula. Sed eget viverra nibh, vitae ornare mauris. Cras quis pretium ligula, vel finibus purus. Etiam commodo augue gravida ullamcorper dictum!</p>
         </article>
+        </div>
+        <div id="smoothie">
+              <img src="img concept/cleanise-decoration-flora-1030909.jpg" alt="smoothie fraise">
+        </div>
+        
     </section>
-    <section class="about-us">
+    <section>
       <h1>Notre équipe ...</h1>
             <span>Un groupe jeune et dynamique à votre service</span>
-            <img src="img concept/IMG_20181219_133835.jpg" alt="notre équipe" id="team">
+            <div id="team">
+            <img src="img concept/IMG_20181219_133835.jpg" alt="notre équipe">
+            </div>
     </section>
+</section>
 </section>
 
 
