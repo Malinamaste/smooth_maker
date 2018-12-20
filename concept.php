74fdc36cdb9e0d14e83056390c21e4a2c3697239
MarieLaure0610 <m.laure.faraut@gmail.com>
2018-12-20 10:39:22 +0100

    concept

--- concept.php
+++ concept.php
@@ -5,43 +5,48 @@
 ?>
 
 <section id="main-background">
- <section class="about-us">
-     <section>
-      <h1>Notre histoire ...</h1>
-            <span>Tout est parti de notre voyage dans les tropiques où la magie est apparue :-D</span>
-        <div id="hawaii">
-            <img src="images/concept/hawaii-maui-mountain-999089.jpg" alt="paysage hawaii">
-        </div>
-        <div id="flex">
-            <div><img src="images/concept/beach-celebrate-cheers-1058433.jpg" alt="smoothie rouge"></div>
-            <p>Ut tempor nisl in enim faucibus, ut venenatis purus volutpat. Vivamus vel posuere diam. In nunc ex, tempor viverra tortor a, ullamcorper elementum ligula. Donec nec turpis ullamcorper, accumsan leo at, placerat diam. Vivamus faucibus pretium pellentesque.</p>
-        </div>
-    </section>
-    <section id="concept">
-       <h1>Notre concept ...</h1>
-       <div id="article">
-        <article>
-            <h2>Du BIO à gogo !</h2>
-              <p>Ut tempor nisl in enim faucibus, ut venenatis purus volutpat. Vivamus vel posuere diam. In nunc ex, tempor viverra tortor a, ullamcorper elementum ligula. Donec nec turpis ullamcorper, accumsan leo at, placerat diam. Vivamus faucibus pretium pellentesque. Donec semper ut arcu vitae laoreet. Proin vitae volutpat risus.</p>
-        </article>
-        <article>
-            <h2>Des recettes ancestrales ...</h2>
-              <p> Pellentesque maximus bibendum nibh at fermentum. Donec dignissim nulla id sem elementum lobortis. Suspendisse auctor tortor in tempor efficitur. Donec lacus diam, tristique at erat vitae, convallis rutrum ligula. Sed eget viverra nibh, vitae ornare mauris. Cras quis pretium ligula, vel finibus purus. Etiam commodo augue gravida ullamcorper dictum!</p>
-        </article>
-        </div>
-        <div id="smoothie">
-              <img src="images/concept/cleanise-decoration-flora-1030909.jpg" alt="smoothie fraise">
-        </div>
-        
-    </section>
-    <section>
-      <h1>Notre équipe ...</h1>
-            <span>Un groupe jeune et dynamique à votre service</span>
-            <div id="team">
-            <img src="images/concept/IMG_20181219_133835.jpg" alt="notre équipe">
-            </div>
-    </section>
-</section>
+	<section id="about-us">
+		<article>
+			<h1>Notre histoire ...</h1>
+			<p class="green">
+				Tout est parti de notre voyage dans les tropiques où la magie est apparue :-D
+            </p>
+			<div id="hawaii"></div>
+		</article>
+		<article>
+			<div id="smoothie"></div>
+			<p>
+				Ut tempor nisl in enim faucibus, ut venenatis purus volutpat. Vivamus vel posuere diam. In nunc ex, tempor viverra tortor a, ullamcorper elementum ligula. Donec nec turpis ullamcorper, accumsan leo at, placerat diam. Vivamus faucibus pretium pellentesque.
+			</p>
+		</article>
+	</section>
+	<section id="concept">
+		<h2>Notre concept ...</h2>
+		<article>
+			<h3>Du BIO à gogo !</h3>
+			<p>
+				Ut tempor nisl in enim faucibus, ut venenatis purus volutpat. Vivamus vel posuere diam. In nunc ex, tempor viverra tortor a, ullamcorper elementum ligula. Donec nec turpis ullamcorper, accumsan leo at, placerat diam. Vivamus faucibus pretium pellentesque. Donec semper ut arcu vitae laoreet. Proin vitae volutpat risus.
+			</p>
+		</article>
+		<article>
+			<h3>Des recettes ancestrales ...</h3>
+			<p>
+				 Pellentesque maximus bibendum nibh at fermentum. Donec dignissim nulla id sem elementum lobortis. Suspendisse auctor tortor in tempor efficitur. Donec lacus diam, tristique at erat vitae, convallis rutrum ligula. Sed eget viverra nibh, vitae ornare mauris. Cras quis pretium ligula, vel finibus purus. Etiam commodo augue gravida ullamcorper dictum!
+			</p>
+		</article>
+		<article id="smoothie-pink">
+			<img src="images/concept/cleanise-decoration-flora-1030909.jpg" alt="smoothie fraise">
+		</article>
+	</section>
+	<section id="team">
+		<h2>Notre équipe ...</h2>
+		<p class="green">
+			Un groupe jeune et dynamique à votre service
+		</p>
+		<article id="photo-team">
+			<img src="images/concept/IMG_20181219_133835.jpg" alt="notre équipe">
+		</article>
+	</section>
 </section>
 
 
